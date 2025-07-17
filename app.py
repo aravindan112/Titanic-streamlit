71302856694392ca11961a43561a481635745f23
aravindan112 <102613210+aravindan112@users.noreply.github.com>
2025-07-17 23:15:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ with open('scaler.pkl', 'rb') as f:
 st.title("Titanic Survival Prediction")
 st.write("Enter the 9 features below (comma-separated):")
 
-Input from user
+#Input from user
 user_input = st.text_input("Enter values (e.g., 3,22,1,0,7.25,0,1,5,2)")
 
 if st.button("Predict"):
